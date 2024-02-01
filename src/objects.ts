e6c654474f2a371256f820172a4b3ad3817ede3b
xKakaxD <ti.demoro@gmail.com>
2024-02-01 18:10:12 -0300

    Introduction for objects

---
+++ src/objects.ts
@@ -0,0 +1,68 @@
+//Type se colocar '? o campo vira opcional'
+type Order = {
+    productId: string;
+    price: number
+}
+
+type User = {
+    firstName: string;
+    age: number;
+    email?: string;
+    password: string;
+    orders: Order[];
+};
+
+const user: User = {
+    firstName: 'Kaká',
+    age: 24,
+    email:'kaka@gmail.com',
+    password: '1234',
+    orders: [{productId: '1', price: 200 }],
+}
+
+const printLog = (message: string) => {}
+
+printLog(user.email!)//a ! diz pro typescript que ela existe sim, que não eh undefined
+
+//Unions unir 2 typer
+type Author = {
+    books: string[]
+}
+
+const author: Author & User = {
+    age: 26,
+    books: ['lorem','ipsum','fake'],
+    firstName: 'Itachi',
+    orders: [],
+    password: '12345'
+};
+
+//Interface READONLY  não deixa alterar depois a variável
+
+interface UserIterface{
+    userName: string;
+    password: string;
+    readonly age: number;
+}
+
+const emailUser: UserIterface = {
+    userName: 'xKakaxD',
+    age: 24,
+    password: '12345'
+};
+
+interface AuthorInterface{
+    books: string[];
+}
+
+const newAuthor: UserIterface & AuthorInterface = {
+    userName: 'xKakaxD',
+    age: 24,
+    password: '12345',
+    books: []
+}
+
+//Uma Diferença entre interface e type, se alterar para interface o bloco a baixo não vai funcionar.
+type Grade = number | string;
+const grade: Grade = 1;
+
